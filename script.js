4c567788520af68a9d5fa879b6537fed3c4fd0ac
NT <36788086+nicolatodor@users.noreply.github.com>
2023-11-23 14:38:27 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+
+document.querySelector(".menu_more").addEventListener('click',function(){
+    var dropdownContent = document.querySelector('.dropdown-content');
+    dropdownContent.style.display = (dropdownContent.style.display === 'block') ? 'none' : 'block';
+
+})
+
+
+
+document.getElementById('1').onclick = function () {
+location.href = "#about";}
+document.getElementById('2').onclick = function () {
+location.href = "#portfolio";}
+document.getElementById('3').onclick = function () {
+location.href = "#services";}
+document.getElementById('4').onclick = function () {
+location.href = "#gallery";}
+document.getElementById('5').onclick = function () {
+location.href = "#contact";}
+document.getElementById('6').onclick = function () {
+location.href = "index.html";}
+
+
+document.getElementById('7').onclick = function () {
+location.href = "#about";}
+document.getElementById('8').onclick = function () {
+location.href = "#portfolio";}
+document.getElementById('9').onclick = function () {
+location.href = "#services";}
+document.getElementById('10').onclick = function () {
+location.href = "#gallery";}
+document.getElementById('11').onclick = function () {
+location.href = "#contact";}
+
+
+function nikolagit() {
+    // Change the URL to the desired destination
+    window.location.href = 'https://github.com/nicolatodor ';
+}
+
+
+function nikolaLinkedin() {
+    // Change the URL to the desired destination
+    window.location.href = 'https://www.linkedin.com/in/nikola-todorovski/ ';
+}
+
+
+
